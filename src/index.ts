ff4b41543739af27fbea0ed2a4e901403056d830
Marcos Wergles <marcospsw96@gmail.com>
2021-06-29 18:23:27 -0300

    feat: tratando errors e adicionando variáveis ambientes

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,32 @@
-import express from 'express';
+import express, { Request, Response, NextFunction } from 'express';
 import cors from 'cors';
 import routes from './routes';
+import { errors } from 'celebrate';
+import AppError from './utils/error';
+import 'dotenv/config';
+
 
 const app = express();
 app.use(cors());
 app.use(express.json());
 app.use(routes);
+app.use(errors());
 
+app.use((err: Error, request: Request, response: Response, _: NextFunction) => {
+  if (err instanceof AppError) {
+    return response.status(err.statusCode).json({
+      status: 'error',
+      message: err.message,
+    });
+  }
 
-const backendPort = 4000;
+  console.error(err);
 
-app.listen(backendPort, () => console.log(`Servidor rodando na porta ${backendPort}`));
+  return response.status(500).json({
+    status: 'error',
+    message: 'Internal server error',
+  });
+});
+
+const port = process.env.SERVER_PORT || 4000;
+app.listen(port, () => console.log(`🚀🚀🚀Servidor rodando na porta ${port}🚀🚀🚀`));
