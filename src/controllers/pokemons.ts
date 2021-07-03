6a6a882061f782e218c38917bc6cb80e73ddeab5
Marcos Wergles <marcospsw96@gmail.com>
2021-07-03 13:05:15 -0300

    feat: filtro de tipos

--- src/controllers/pokemons.ts
+++ src/controllers/pokemons.ts
@@ -7,13 +7,8 @@ const pokemonsRouter = Router();
 pokemonsRouter.get(
   '/', async (req, res) => {
     const { url } = req.query;
-    // const stringUrl = url.toString();
-    // const stringOffset = offset.toString();
-    // const stringLimit = limit.toString();
-    // const totalUrl = stringUrl.concat(stringOffset, stringLimit);
 
     async function getPokemons(){
-      // let { data } = await axios('https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898');
       const { data } = await axios.get(`${url}`);
       const pokemons = data.results;
       return {
@@ -37,15 +32,60 @@ pokemonsRouter.get(
     }
   
     try {
-      const { pokemons, next_url } = await getPokemons();
+      let { pokemons, next_url } = await getPokemons();
       const completePokemons = await Promise.all(pokemons.map(async (pokemon: Pokemon): Promise<Object> => {
         const completePokemon = await getPokemon(pokemon.url);
+        if(completePokemon.id === 898) {
+          next_url = null;
+        }
+        if(completePokemon.id > 898) {
+          return null;
+        }
         return completePokemon;
       }));    
       return res.json({
         next_url,
-        completePokemons
+        completePokemons,
       });
+    } catch (error) {
+      console.log(error);
+    }
+});
+
+pokemonsRouter.get(
+  '/complete', async (req, res) => {
+    const { type } = req.query;
+
+    async function getPokemons(){
+      const { data } = await axios.get(`https://pokeapi.co/api/v2/type/${type}`);  
+      const pokemons = data.pokemon;
+      return pokemons;    
+    }
+  
+    async function getPokemon(url: string) {
+      const { data } = await axios.get(url);
+
+      return {
+        url,
+        id: data.id,
+        name: data.name,
+        sprite: data.sprites.other.dream_world.front_default 
+          ? data.sprites.other.dream_world.front_default 
+          : data.sprites.other['official-artwork'].front_default,
+        types: data.types,
+      };
+    }
+  
+    try {
+      const pokemons = await getPokemons();
+      const completePokemons = await Promise.all(pokemons.map(async (pokemon): Promise<Object> => {
+        const completePokemon = await getPokemon(pokemon.pokemon.url);        
+        if(completePokemon.id > 898) {
+          return null;
+        }
+        return completePokemon;
+      }));    
+      return res.json(completePokemons);
     } catch (error) {
       console.log(error);
     }
@@ -66,13 +106,3 @@ pokemonsRouter.get('/id', async (req, res) => {
 });
 
 export default pokemonsRouter;
-
-//CASO PRECISE DE PAGINAÇÃO
-
-// while (data.next !== null) {
-//   const response = await axios(data.next);
-//   response.data.results.map(pokemon => {
-//     return pokemons.push(pokemon);
-//   });
-//   data.next = response.data.next;
-// }
