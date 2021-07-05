5742de1492f0f2eaf39e015cd264bacd59d7cf93
Marcos Wergles <marcospsw96@gmail.com>
2021-07-05 17:10:10 -0300

    feat: filtro de nomes e ids e conserto de alguns bugs

--- src/controllers/pokemons.ts
+++ src/controllers/pokemons.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 import { Router } from "express";
-import { Pokemon } from "../models/pokemon";
+import { CompletePokemon, PokemonsByType } from "../models/pokemon";
 
 const pokemonsRouter = Router();
 
@@ -19,21 +19,22 @@ pokemonsRouter.get(
   
     async function getPokemon(url: string) {
       const { data } = await axios.get(url);
+      const pokemon: CompletePokemon = data;
 
       return {
         url,
-        id: data.id,
-        name: data.name,
-        sprite: data.sprites.other.dream_world.front_default 
-          ? data.sprites.other.dream_world.front_default 
-          : data.sprites.other['official-artwork'].front_default,
-        types: data.types,
+        id: pokemon.id,
+        name: pokemon.name,
+        sprite: pokemon.sprites.other.dream_world.front_default 
+          ? pokemon.sprites.other.dream_world.front_default 
+          : pokemon.sprites.other['official-artwork'].front_default,
+        types: pokemon.types,
       };
     }
   
     try {
       let { pokemons, next_url } = await getPokemons();
-      const completePokemons = await Promise.all(pokemons.map(async (pokemon: Pokemon): Promise<Object> => {
+      const completePokemons = await Promise.all(pokemons.map(async (pokemon: CompletePokemon): Promise<Object> => {
         const completePokemon = await getPokemon(pokemon.url);
         if(completePokemon.id === 898) {
           next_url = null;
@@ -42,10 +43,11 @@ pokemonsRouter.get(
           return null;
         }
         return completePokemon;
-      }));    
+      }));
+      
       return res.json({
         next_url,
-        completePokemons,
+        completePokemons: completePokemons.filter(Boolean),
       });
     } catch (error) {
       console.log(error);
@@ -53,7 +55,7 @@ pokemonsRouter.get(
 });
 
 pokemonsRouter.get(
-  '/complete', async (req, res) => {
+  '/bytype', async (req, res) => {
     const { type } = req.query;
 
     async function getPokemons(){
@@ -64,41 +66,98 @@ pokemonsRouter.get(
   
     async function getPokemon(url: string) {
       const { data } = await axios.get(url);
+      const pokemon: CompletePokemon = data;
 
       return {
         url,
-        id: data.id,
-        name: data.name,
-        sprite: data.sprites.other.dream_world.front_default 
-          ? data.sprites.other.dream_world.front_default 
-          : data.sprites.other['official-artwork'].front_default,
-        types: data.types,
+        id: pokemon.id,
+        name: pokemon.name,
+        sprite: pokemon.sprites.other.dream_world.front_default 
+          ? pokemon.sprites.other.dream_world.front_default 
+          : pokemon.sprites.other['official-artwork'].front_default,
+        types: pokemon.types,
       };
     }
   
     try {
       const pokemons = await getPokemons();
-      const completePokemons = await Promise.all(pokemons.map(async (pokemon): Promise<Object> => {
+      const completePokemons = await Promise.all(pokemons.map(async (pokemon: PokemonsByType): Promise<Object> => {
         const completePokemon = await getPokemon(pokemon.pokemon.url);        
         if(completePokemon.id > 898) {
           return null;
         }
         return completePokemon;
-      }));    
+      }));
+
+      return res.json(completePokemons.filter(Boolean));
+    } catch (error) {
+      console.log(error);
+    }
+});
+
+pokemonsRouter.get(
+  '/filter', async (req, res) => {
+    const { search } = req.query;
+    const valor = search.toString();
+    const isNumber = parseInt(valor);
+
+    async function getPokemons(){
+      const { data } = await axios.get(`https://pokeapi.co/api/v2/pokemon/?offset=0&limit=898`);  
+      const pokemons = data.results;
+      return pokemons;    
+    }
+  
+    async function getPokemon(url: string) {
+      const { data } = await axios.get(url);
+      const pokemon: CompletePokemon = data;
+
+      return {
+        url,
+        id: pokemon.id,
+        name: pokemon.name,
+        sprite: pokemon.sprites.other.dream_world.front_default 
+          ? pokemon.sprites.other.dream_world.front_default 
+          : pokemon.sprites.other['official-artwork'].front_default,
+        types: pokemon.types,
+      };
+    }
+  
+    try {
+      const pokemons = await getPokemons();
+      const filteredPokemons = pokemons.map((pokemon: CompletePokemon) => {
+        if(isNaN(isNumber) && pokemon.name.includes(valor)){
+          return pokemon;
+        }
+        const last4Numbers = pokemon.url.slice(-4);
+        if(!isNaN(isNumber) && last4Numbers.includes(valor)){
+          return pokemon;     
+        }
+        return null;
+      });
+
+      const completePokemons = await Promise.all(filteredPokemons.filter(Boolean).map(async (pokemon: CompletePokemon): Promise<Object> => {
+        const completePokemon = await getPokemon(pokemon.url);   
+        return completePokemon;  
+      }));
+      
       return res.json(completePokemons);
     } catch (error) {
       console.log(error);
     }
 });
 
+
+
 pokemonsRouter.get('/id', async (req, res) => {
   try {
     let { data } = await axios(`https://pokeapi.co/api/v2/pokemon/${req.query.pokemon}/`);
-    const pokemon = data;
+    const pokemon: CompletePokemon = data;
   
     return res.json({
       name: pokemon.name,
-      sprite: pokemon.sprites.other.dream_world.front_default,
+      sprite: data.sprites.other.dream_world.front_default 
+      ? data.sprites.other.dream_world.front_default 
+      : data.sprites.other['official-artwork'].front_default,
     });
   } catch (error) {
     console.log(error);
--- src/models/pokemon.ts
+++ src/models/pokemon.ts
@@ -1,5 +1,29 @@
-export interface Pokemon { 
-  name: string,
-  url?: string,
-  sprite?: string,  
+export interface CompletePokemon {
+  url: string,
+  id: number,
+  name: string,  
+  sprites: {
+    other: {
+      dream_world: {
+        front_default: string,
+      },
+    },
+  },
+  types: Types[],
 }
+
+export interface Types {
+  slot: number,
+  type: {
+    name: string,
+    url: string,
+  }
+}
+
+export interface PokemonsByType {
+  pokemon: {
+    name: string,
+    url: string,
+  }
+}
+
