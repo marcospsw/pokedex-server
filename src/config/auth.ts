f3bce627461ea468b397d39ec111e03b215c290f
Marcos Wergles <marcospsw96@gmail.com>
2021-08-05 09:29:14 -0300

    fix: ajustando variaveis ambiente

--- src/config/auth.ts
+++ src/config/auth.ts
@@ -1,5 +1,5 @@
 import 'dotenv/config';
 export const config = {
-	secret: 'f76427134b7862141dec15db3bfee68eccf917adada7659d82f77902dd0b880b',
-	expiresIn: '6h',
+	secret: process.env.TOKEN_SECRET,
+	expiresIn: process.env.TOKEN_EXPIRES,
 };
--- src/config/connection.ts
+++ src/config/connection.ts
@@ -1,14 +1,15 @@
 import moongose from 'mongoose';
+import 'dotenv/config';
 
 const connection = moongose
-	.connect('mongodb+srv://admin:admin@pokedexdatabase.tu8um.mongodb.net/pokedex-database?retryWrites=true&w=majority', {
+	.connect(process.env.MONGODB_URL, {
 		useNewUrlParser: true,
 		useUnifiedTopology: true,
 		useFindAndModify: false,
 		useCreateIndex: true,
 	})
 	.then(() => {
-		console.log('Conexão com o Banco de Dados estabelecida com sucesso!');
+		console.log('📁📁📁Conexão com o Banco de Dados estabelecida com sucesso!📁📁📁');
 	})
 	.catch((error) => {
 		console.log('Erro ao estabelecer conexão com o Banco de Dados', error);
